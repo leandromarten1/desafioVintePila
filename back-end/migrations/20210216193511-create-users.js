d35954496ae8626ea4f805056f031ff89d73459b
leandromarten1 <leandromarten1@gmail.com>
2021-02-16 16:36:07 -0300

    create user migration

---
+++ back-end/migrations/20210216193511-create-users.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const UsersTable = queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      sobrenome: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      telefone: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      cpf: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+
+    return UsersTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable('Users'),
+};
